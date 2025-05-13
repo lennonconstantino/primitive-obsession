039dc1615f5ba411643244ab194881accf8a94b2
lennonconstantino <lennonconstantino@gmail.com>
2025-05-13 18:52:53 -0300

    doing some test in main

--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
+	"primitive-obsession/src/module"
 )
 
 func main() {
 	fmt.Println("Entry code here.")
 
 	// Client Code
+	var email module.Email
+	email.Email = "lennon@email.com"
+	var cpf module.Cpf
+	cpf.Cpf = "123.456.789-09"
+	var age module.Age
+	age.Age = 36
+
+	if erro := email.Validation(); erro != nil {
+		fmt.Println("Error domain. ->", erro)
+		return
+	}
+
+	if erro := cpf.ValidationCpf(); erro != nil {
+		fmt.Println("Error domain. ->", erro)
+		return
+	}
+
+	if erro := age.Validate(); erro != nil {
+		fmt.Println("Error domain. ->", erro)
+		return
+	}
+
+	var customer module.Customer
+	customer.Email = email
+	customer.Cpf = cpf
+	customer.Age = age
 
 	fmt.Println("Customer info: ")
+	fmt.Println("-- Email: ", customer.GetEmail())
+	fmt.Println("-- Cpf: ", customer.GetCpf())
+	fmt.Println("-- Age: ", customer.GetAge())
 }
